f40dafa0488cc040ef5fc1a58a4bebe7247ebd24
cata <rebaza98@gmail.com>
2022-12-27 21:19:28 -0500

    with data table an record form

--- force-app/main/default/lwc/customContactAccountList/customContactAccountList.js
+++ force-app/main/default/lwc/customContactAccountList/customContactAccountList.js
@@ -1,10 +1,22 @@
 import { LightningElement, wire, api, track } from 'lwc';
 import getContactListFiltered from '@salesforce/apex/compactContactAccountController.getContactListFiltered';
 import {refreshApex} from '@salesforce/apex';
+//OBJECT
+import CONTACT_OBJECT from '@salesforce/schema/Contact'
+//FIELDS
+import FIRST_NAME_FIELD from '@salesforce/schema/Contact.FirstName'
+import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName'
+import TITLE_FIELD from '@salesforce/schema/Contact.Title'
+import PHONE_FIELD from '@salesforce/schema/Contact.Phone'
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import ACCOUNT_FIELD from '@salesforce/schema/Contact.AccountId';
+
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class CustomContactAccountList extends LightningElement {
     @api recordId
     @api objectApiName
+    contactObject = CONTACT_OBJECT
     showAccountContactForm = false
     contactList 
     wiredContactResult
@@ -23,17 +35,60 @@ export default class CustomContactAccountList extends LightningElement {
 
    //NEW CONTACT
    newContactHandler(){
+    console.log("OBJECT ) ", CONTACT_OBJECT)
+    console.log("API NAME = ", this.objectApiName)
     this.showAccountContactForm = true
    }
-   successHandler(){
+   successHandler(event){
+    console.log("SUCCESS SHLU TRIGGER")
     refreshApex(this.wiredContactResult)
     this.showAccountContactForm = false;
+    this.dispatchEvent(
+        new ShowToastEvent({
+            title: 'Success',
+            message:`Contact ${event.detail.fields.FirstName.value} ${event.detail.fields.LastName.value} Created`,
+            variant: 'success',
+        }),
+    );
+    console.log("CVENET SUCCESS = ", event)
+    
     
    }
    cancelHandler(){
     this.showAccountContactForm = false;
    }
 
+   accountContactColumns = [
+    //{ label:'Contact Id', fieldName:'Id', hideLabel:true},
+    { label:'Name', fieldName:'Name', hideDefaultActions:true},
+    { label:'Title', fieldName:'Title', hideDefaultActions:true},
+    { label:'Email', fieldName:'Email', type:'email', hideDefaultActions:true},
+    { label:'Phone', fieldName:'Phone', type:'phone', hideDefaultActions:true},
+    
+    ]
+
+    //MODAL POP UP
+    objectName = CONTACT_OBJECT
+    
+    
+    fieldList = [FIRST_NAME_FIELD, LAST_NAME_FIELD, TITLE_FIELD, PHONE_FIELD, EMAIL_FIELD]
+
+    submitHandler(event) {
+        event.preventDefault(); 
+        let fields = event.detail.fields;
+        fields[ACCOUNT_FIELD.fieldApiName] = this.recordId //Force to link the current AccountID
+        try {
+            this.template.querySelector('lightning-record-form').submit(fields);
+        }catch(error){
+            new ShowToastEvent({
+                title: 'Error',
+                message:'Something happened please try again',
+                variant: 'error',
+            })
+        }
+        
+    }
+ 
 
 
 }
